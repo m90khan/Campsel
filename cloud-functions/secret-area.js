1298006dbaede85b4a51a9c7f4a1a41c9e7b119c
Khan Mohsin <m90khan@gmail.com>
2020-08-13 16:49:04 +0500

    netlify update cloud function

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,6 +1,19 @@
 exports.handler = function (event, context, callback) {
-  callback(null, {
-    statusCode: 200,
-    body: "Welcome to secret Area",
-  });
+  let body;
+  const secret = `<h3>Welcome to Secret Area</h3>`;
+  if (event.body) {
+    body = JSON.parse(event.body);
+  } else {
+    body = {};
+  }
+  if (body.password == "javascript") {
+    callback(null, {
+      statusCode: 200,
+      body: secretContent,
+    });
+  } else {
+    callback(null, {
+      statusCode: 401,
+    });
+  }
 };
