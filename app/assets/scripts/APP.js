4cd136608ca329136528a62a5ae1b3c74fd37cc9
Khan Mohsin <m90khan@gmail.com>
2020-08-13 17:08:31 +0500

    cloud func add complete

--- app/assets/scripts/APP.js
+++ app/assets/scripts/APP.js
@@ -4,6 +4,10 @@ import "lazysizes";
 import Navigation from "../scripts/modules/navigation";
 import TourPackage from "../scripts/modules/tourPackage";
 import stickyHeader from "../scripts/modules/stickyHeader";
+
+import ClientArea from "./modules/ClientArea";
+
+new ClientArea();
 const navMenu = new Navigation();
 new stickyHeader();
 new TourPackage(document.querySelectorAll(".package"));
