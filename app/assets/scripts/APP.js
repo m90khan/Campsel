7f9c9c70262787607a562a154850d7de3a022920
Khan Mohsin <m90khan@gmail.com>
2020-08-14 13:55:09 +0500

    clearing aws lamda

--- app/assets/scripts/APP.js
+++ app/assets/scripts/APP.js
@@ -5,15 +5,6 @@ import Navigation from "../scripts/modules/navigation";
 import TourPackage from "../scripts/modules/tourPackage";
 import stickyHeader from "../scripts/modules/stickyHeader";
 
-import ClientArea from "./modules/ClientArea";
-
-// Testing React Integration
-import React from "react";
-import ReactDOM from "react-dom";
-import Card from "./modules/reactCard";
-ReactDOM.render(<Card />, document.querySelector("#react-test"));
-
-new ClientArea();
 const navMenu = new Navigation();
 new stickyHeader();
 new TourPackage(document.querySelectorAll(".package"));
@@ -21,3 +12,22 @@ new TourPackage(document.querySelectorAll(".feature"));
 if (module.hot) {
   module.hot.accept();
 }
+
+// Testing React Integration
+/*
+* React is integrated . help later when scaling 
+
+import React from "react";
+import ReactDOM from "react-dom";
+import Card from "./modules/reactCard";
+ReactDOM.render(<Card />, document.querySelector("#react-test"));
+*/
+
+/*
+ Topic: to implement AWS Lamda for later  
+*test is complete wit netlify
+import ClientArea from "./modules/ClientArea";
+
+new ClientArea();
+
+*/
--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,19 +1,21 @@
-exports.handler = function (event, context, callback) {
-  let body;
-  const secret = `<h3>Welcome to Secret Area</h3>`;
-  if (event.body) {
-    body = JSON.parse(event.body);
-  } else {
-    body = {};
-  }
-  if (body.password == "javascript") {
-    callback(null, {
-      statusCode: 200,
-      body: secret,
-    });
-  } else {
-    callback(null, {
-      statusCode: 401,
-    });
-  }
-};
+// * to let netlify run the lamda
+
+// exports.handler = function (event, context, callback) {
+//   let body;
+//   const secret = `<h3>Welcome to Secret Area</h3>`;
+//   if (event.body) {
+//     body = JSON.parse(event.body);
+//   } else {
+//     body = {};
+//   }
+//   if (body.password == "javascript") {
+//     callback(null, {
+//       statusCode: 200,
+//       body: secret,
+//     });
+//   } else {
+//     callback(null, {
+//       statusCode: 401,
+//     });
+//   }
+// };
